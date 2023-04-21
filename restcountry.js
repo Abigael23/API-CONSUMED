be6554fe119627ae12483c2ead96e42434c4b96f
Abigael Daramola <abigaeldaramola2016@gmail'com>
2023-04-21 12:30:09 +0100

    second commit

--- restcountry.js
+++ restcountry.js
@@ -4,7 +4,7 @@ async function dispCountry() {
     console.log(result);
     result.forEach((element, index) => {
         let contName = element.name.common
-        let languages =element.languages
+        let languages = element.languages
         console.log(contName);
         showresult.innerHTML +=`
         <tbody>
@@ -13,7 +13,7 @@ async function dispCountry() {
             <td>${contName}</td>
             <td>${element.capital}</td>
             <td>${element.population}</td>
-            <td>${languages}</td>
+            <td>${JSON.stringify(languages)}</td>
             <td>${element.independent}</td>
             </tr>
         </tbody>
@@ -24,8 +24,54 @@ async function dispCountry() {
     dispCountry()
     // note: <td>${element.name.common}</td> or <td>${contName}</td> can be used
 
-    function Independent() {
-        
-        // let independent = result.filter((independent)=> (element.independent) == "true") 
-        dispCountry()  
+
+    async function Independent() {
+    let allCont = await fetch("https://restcountries.com/v3.1/all");
+    let result = await allCont.json();
+    let independentcountry = result.filter((country)=> (country.independent) == true);
+    console.log(independentcountry);
+    showresult.innerHTML =""
+    independentcountry.forEach((element, index) => {
+        // let contName = element.name.common
+        // let languages = element.languages
+        // console.log(contName);
+        showresult.innerHTML +=`
+        <tbody>
+            <tr class="">
+            <td>${index+1}</td>
+            <td>${element.name.common}</td>
+            <td>${element.capital}</td>
+            <td>${element.population}</td>
+            <td>${JSON.stringify(element.languages)}</td>
+            <td>${element.independent}</td>
+            </tr>
+        </tbody>
+    `
+     });
     }
+    // Independent()
+
+    async function NonIndependent() {
+        let allCont = await fetch("https://restcountries.com/v3.1/all");
+        let result = await allCont.json();
+        let nonindependentcountry = result.filter((element)=> (element.independent) == false);
+        console.log(nonindependentcountry);
+        showresult.innerHTML =""
+        nonindependentcountry.forEach((element, index) => {
+            let contName = element.name.common
+            let languages = element.languages
+            console.log(contName);
+            showresult.innerHTML +=`
+            <tbody>
+                <tr class="">
+                <td>${index+1}</td>
+                <td>${contName}</td>
+                <td>${element.capital}</td>
+                <td>${element.population}</td>
+                <td>${JSON.stringify(languages)}</td>
+                <td>${element.independent}</td>
+                </tr>
+            </tbody>
+        `
+         });
+        }
